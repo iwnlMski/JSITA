8bab76888b90cba8b77ad7d244c7bb67265bd130
iwnlMski <mski.python@gmail.com>
2021-03-22 15:35:52 +0100

    Fix templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(response):
-    return render(response, '', context={})
+    return render(response, 'mainapp/index.html', context={})
+
+
+def products(response):
+    return render(response, 'mainapp/products.html', context={})
+
+
+def accounts(response):
+    return render(response, 'mainapp/accounts.html', context={})
+
